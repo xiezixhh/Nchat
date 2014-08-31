1309561c5715d0fb7829c7f47a3db8df3a23b0f3
xhh261672 <563249383@qq.com>
2014-08-31 21:15:39 +0800

    ee

--- app.js
+++ app.js
@@ -91,14 +91,28 @@ var server = app.listen(app.get('port'), function() {
 var io = require('socket.io').listen(server);
 io.sockets.on('connection', function(socket){
     socket.on('online', function (data) {
-  //将上线的用户名存储为 socket 对象的属性，以区分每个 socket 对象，方便后面使用
-    socket.name = data.user;
-  //users 对象中不存在该用户名则插入该用户名
-    if (!users[data.user]) {
-        users[data.user] = data.user;
-     }
-  //向所有用户广播该用户上线信息
-    io.sockets.emit('online', {users: users, user: data.user});
-});
+      //将上线的用户名存储为 socket 对象的属性，以区分每个 socket 对象，方便后面使用
+        socket.name = data.user;
+      //users 对象中不存在该用户名则插入该用户名
+        if (!users[data.user]) {
+            users[data.user] = data.user;
+         }
+      //向所有用户广播该用户上线信息
+        io.sockets.emit('online', {users: users, user: data.user});
+    });
+
+    socket.on('say', function(data){
+        if(data.to == 'all'){
+            socket.broadcast.emit('say', data);
+        }else{
+            var clients = io.sockets.clients();
+
+            clients.forEach(function(client){
+                if(client.name == data.to){
+                    socket.emit('say', data);
+                }
+            });
+        }
+    });
 });
  //module.exports = app;
--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -5,22 +5,49 @@ $(document).ready(function(){
 	var to = 'all';//设置默认接收对象为"所有人"
 	//发送用户上线信号
 	socket.emit('online', {user: from});
-		socket.on('online', function (data) {
-		//显示系统消息
-		if (data.user != from) {
-		var sys = '<div style="color:#f00">系统(' + now() + '):' + '用户 ' + data.user + ' 上线了！</div>';
-		} else {
-		var sys = '<div style="color:#f00">系统(' + now() + '):你进入了聊天室！</div>';
-		}
-		$("#contents").append(sys + "<br/>");
-		//刷新用户在线列表
-		flushUsers(data.users);
-		//显示正在对谁说话
-		showSayTo();
-	});
-});
+	socket.on('online', function (data) {
+  	//显示系统消息
+  	if (data.user != from) {
+  	var sys = '<div style="color:#f00">系统(' + now() + '):' + '用户 ' + data.user + ' 上线了！</div>';
+  	} else {
+  	var sys = '<div style="color:#f00">系统(' + now() + '):你进入了聊天室！</div>';
+  	}
+  	$("#contents").append(sys + "<br/>");
+  	//刷新用户在线列表
+  	flushUsers(data.users);
+  	//显示正在对谁说话
+  	showSayTo();
+  });
 
-//刷新用户在线列表
+  socket.on('say', function(data){
+     if(data.to == 'all'){
+        $("#contents").append('<div>' + data.from + '(' + now() + ')对 所有人 说：<br/>' + data.msg + '</div><br />');
+     }
+
+     if(data.to == from) {
+        $("#contents").append('<div style="color:#00f" >' + data.from + '(' + now() + ')对 你 说：<br/>' + data.msg + '</div><br />');
+     }
+  });
+
+  $('#say').click(function(){
+    var $msg = $('#input_content').html();
+    //alert(msg);
+    if($msg == '')
+      return;
+
+    if(to == 'all'){
+      $('#contents').append('<div>你(' + now() + ')对 所有人 说：<br/>' + $msg + '</div><br />');
+    }
+    else{
+      $('#contents').append('<div>你(' + now() + ')对'+to+' 说：<br/>' + $msg + '</div><br />')
+    }
+
+    socket.emit('say', {from: from, to: to, msg: $msg});
+
+    $('#input_content').html('').focus();
+  });
+
+  //刷新用户在线列表
 function flushUsers(users) {
   //清空之前用户列表，添加 "所有人" 选项并默认为灰色选中效果
   $("#list").empty().append('<li title="双击聊天" alt="all" class="sayingto" onselectstart="return false">所有人</li>');
@@ -55,3 +82,6 @@ function now() {
   var time = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + (date.getMinutes() < 10 ? ('0' + date.getMinutes()) : date.getMinutes()) + ":" + (date.getSeconds() < 10 ? ('0' + date.getSeconds()) : date.getSeconds());
   return time;
 }
+
+});
+
